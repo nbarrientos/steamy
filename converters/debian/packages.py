ab270bfb34aa195b028d0e27248ec26ceada7090
Nacho Barrientos <nacho@criptonita.com>
2009-12-21 20:44:56 +0100

    Add output and baseURI optargs

--- converters/debian/packages.py
+++ converters/debian/packages.py
@@ -8,36 +8,58 @@ from rdflib.Graph import ConjunctiveGraph
 from parser import Parser
 from export import Triplifier, Serializer
 
+VERSION = "0.1alpha"
+
 def init():
   configLogger()
   parseArgs()
 
 def configLogger():
-  logging.basicConfig(level=logging.DEBUG)
+  logging.basicConfig(level=logging.DEBUG, format='%(levelname)s: %(message)s')
 
 def parseArgs():
-  usage = "usage: %prog [options]"
-  parser = OptionParser(usage)
-  parser.add_option("-p", "--packages", dest="packages", help="read Packages from FILENAME")
-  parser.add_option("-s", "--source", dest="sources", help="read Sources from FILENAME")
-
+  parser = OptionParser(usage="%prog [options]", version="%prog " + VERSION)
+  parser.add_option("-p", "--packages", dest="packages",\
+                    metavar="FILE", help="read Packages from FILE")
+  parser.add_option("-s", "--sources", dest="sources",\
+                    metavar="FILE", help="read Sources from FILE")
+  parser.add_option("-b", "--baseURI", dest="baseURI",\
+                    metavar="URI", help="use URI as base URI for all resources")
+  parser.add_option("-P", "--packages-output", dest="packagesOutput",\
+                    default="Packages.rdf",\
+                    metavar="FILE", help="print rdflized Packages to FILE [default: %default]")
+  parser.add_option("-S", "--sources-output", dest="sourcesOutput",\
+                    default="Sources.rdf",\
+                    metavar="FILE", help="print rdflized Sources to FILE [default: %default]")
   (options, args) = parser.parse_args()
 
+  # FIXME: Add more checks
+  if not options.packages and not options.sources:
+    logging.info("Nothing to do, did you forget -p and/or -s?")
+
   if options.packages:
-    logging.debug("Parsing package data from %s" % options.packages)
-    processPackages(options.packages)
+    if options.baseURI:
+      logging.debug("Converting packages metadata from %s to %s" % \
+                    (options.packages, options.packagesOutput))
+      processPackages(options.packages, options.packagesOutput, options.baseURI)
+    else:
+      logging.error("Base URI is missing, did you forget '-b'?")
+      exit(-1)
+
+  if options.sources:
+    pass # FIXME
 
 # FIMXE: Use a class instead
-def processPackages(packagesFile):
+def processPackages(src, out, baseURI):
 
   # FIXME: Refactor to init
   graph = ConjunctiveGraph()
 
-  inputFile = openInputFile(packagesFile)
-  outputFile = openOutputFile("Packages.rdf") # FIXME
+  inputFile = openInputFile(src)
+  outputFile = openOutputFile(out)
 
   parser = Parser()
-  triplifier = Triplifier(graph, "http://debian.org") # FIXME: as user input
+  triplifier = Triplifier(graph, baseURI)
   serializer = Serializer()
 
   rawPackages = deb822.Packages.iter_paragraphs(inputFile)
@@ -53,20 +75,20 @@ def processPackages(packagesFile):
   serializer.serializeToFile(graph, outputFile)
 
   inputFile.close()
-  outputFile.flush()
   outputFile.close()
 
 def openInputFile(path):
   try:
     return open(path, "r")
   except:
-    print 'Error opening input file for reading.'
+    log.error("Error opening input file for reading")
 
 def openOutputFile(path):
   try:
+    # TODO: does the file already exist?
     return open(path, "w+")
   except:
-    print 'Error opening output file for writting.'
+    log.error("Error opening output file for writting")
 
 if __name__ == "__main__":
   init()
