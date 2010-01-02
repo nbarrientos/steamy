1e43683c160eb090c0d4a137890cf2eb5635cdcf
Nacho Barrientos <nacho@criptonita.com>
2010-01-02 22:45:06 +0100

    Support à-la-JUnit XML-formatted test results

--- converters/debian/runtests.py
+++ converters/debian/runtests.py
@@ -1,12 +1,37 @@
+from optparse import OptionParser
 import unittest
+import xmlrunner
 
 from test.unit import modelsTest, parserTest, exportTest
 
-def launchTests():
-  suite = unittest.TestLoader().loadTestsFromModule(modelsTest)
-  suite.addTests(unittest.TestLoader().loadTestsFromModule(parserTest))
-  suite.addTests(unittest.TestLoader().loadTestsFromModule(exportTest))
-  unittest.TextTestRunner(verbosity=2).run(suite)
+class TestRunner:
+  def __init__(self):
+    self.out = None
+
+  def config(self):
+    parser = OptionParser()
+    parser.add_option("-x", "--xml", action="store_true", dest="xml",\
+                      default=False, help="switch to XML output")
+    parser.add_option("-o", "--output", dest="output",\
+                      metavar="FILE", help="dump XML to FILE")
+
+    (self.opts, args) = parser.parse_args()
+
+    if self.opts.output:
+      self.out = open(self.opts.output, "w")
+
+    return self
+
+  def run(self):
+    suite = unittest.TestLoader().loadTestsFromModule(modelsTest)
+    suite.addTests(unittest.TestLoader().loadTestsFromModule(parserTest))
+    suite.addTests(unittest.TestLoader().loadTestsFromModule(exportTest))
+    if self.opts.xml:
+      xmlrunner.XMLTestRunner(self.out).run(suite)
+      if self.out:
+        self.out.close()
+    else:
+      unittest.TextTestRunner(verbosity=2).run(suite)
 
 if __name__ == '__main__':
-    launchTests()
+    TestRunner().config().run()
