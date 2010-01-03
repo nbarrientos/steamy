3358576cce3dd10c171ce5788d08f7bfdea564a9
Nacho Barrientos <nacho@criptonita.com>
2010-01-03 23:41:57 +0100

    Add setup script and launcher script

---
+++ converters/debian/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+from distutils.core import setup
+
+setup(name='romeo',
+      version='1.0',
+      description='RDFizer for Debian Packages Archive',
+      author='Nacho Barrientos Arias',
+      author_email='nacho@debian.org',
+      py_modules=['decorators', 'parsers', 'export', 'errors', 'models', 'launcher'],
+      scripts=['romeo']
+      )
+
