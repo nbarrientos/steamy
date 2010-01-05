ddf6c07dfe784adbe04a09f9422d9b7f2cc21cfc
Nacho Barrientos <nacho@criptonita.com>
2010-01-05 17:19:48 +0100

    Treat distributions individually in order to cherry pick wanted architectures

--- converters/debian/tools/fetch-debian-archive.sh
+++ converters/debian/tools/fetch-debian-archive.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
+# fetch-debian-archive.sh
+#
+# Diego Berrueta <diego@berrueta.net>
+# Nacho Barrientos Arias <nacho@debian.org>
 
-ARCHIVED_DISTS="slink potato woody sarge"
-CURRENT_DISTS="etch lenny"
-ARCHS="i386"
+# License: MIT
+
 AREAS="main non-free contrib"
-OUTPUT_DIR="dists"
+OUTPUT_DIR=$1
 
-function processDists {
-  for dist in $@; do
-    echo "Will download $dist (arches: $ARCHS)"
-    for area in $AREAS; do
-        echo "Area: $area"
-        mkdir -p $OUTPUT_DIR/$dist/$area/source
-        SOURCES_URL="$BASE_URL/$dist/$area/source/Sources.gz"
-        OUTPUT_PATH="$OUTPUT_DIR/$dist/$area/source/Sources.gz"
-        processFile $SOURCES_URL $OUTPUT_PATH
-        for arch in $ARCHS; do
-            mkdir -p $OUTPUT_DIR/$dist/$area/binary-$arch
-            PACKAGES_URL="$BASE_URL/$dist/$area/binary-$arch/Packages.gz"
-            OUTPUT_PATH="$OUTPUT_DIR/$dist/$area/binary-$arch/Packages.gz"
-            processFile $PACKAGES_URL $OUTPUT_PATH
-          done
-    done
+if [ -z $1 ]
+then
+  echo "Usage $0 <dest_dir>"
+  exit -1
+fi
+
+function processDist {
+  # $1: Distname
+  echo "Will download $1 (arches: $ARCHS)"
+  for area in $AREAS; do
+      echo "Area: $area"
+      mkdir -p $OUTPUT_DIR/$1/$area/source
+      SOURCES_URL="$BASE_URL/$1/$area/source/Sources.gz"
+      OUTPUT_PATH="$OUTPUT_DIR/$1/$area/source/Sources.gz"
+      processFile $SOURCES_URL $OUTPUT_PATH
+      for arch in $ARCHS; do
+          mkdir -p $OUTPUT_DIR/$1/$area/binary-$arch
+          PACKAGES_URL="$BASE_URL/$1/$area/binary-$arch/Packages.gz"
+          OUTPUT_PATH="$OUTPUT_DIR/$1/$area/binary-$arch/Packages.gz"
+          processFile $PACKAGES_URL $OUTPUT_PATH
+        done
   done
 }
 
@@ -29,15 +37,36 @@ function processFile {
   # $1: Download URL $2: Output file
   echo "Downloading file: $1..."
   wget $1 -O $2 &> /dev/null
-  local OUTPUT_PATH_UNZIPPED=$(echo $2 |sed -e "s/.gz//")
+  OUTPUT_PATH_UNZIPPED=$(echo $2 |sed -e "s/.gz//")
   echo "Gunzipping and recoding $2 to $OUTPUT_PATH_UNZIPPED..."
   gzip -d $2 -c > $OUTPUT_PATH_UNZIPPED
   iconv -f "ISO-8859-1" -t "UTF-8" $OUTPUT_PATH_UNZIPPED > $OUTPUT_PATH_UNZIPPED.u
   mv $OUTPUT_PATH_UNZIPPED.u $OUTPUT_PATH_UNZIPPED
 }
- 
+
+# Old distributions
+
 BASE_URL="http://ftp.de.debian.org/archive/debian/dists"
-processDists $ARCHIVED_DISTS
+ARCHS="i386 m68k"
+processDist "hamm"
+
+ARCHS="i386 m68k sparc"
+processDist "slink"
+
+ARCHS="i386"
+processDist "potato"
+
+ARCHS="i386"
+processDist "woody"
+
+ARCHS="i386"
+processDist "sarge"
+
+# Current ones
 
 BASE_URL="http://ftp.de.debian.org/debian/dists"
-processDists $CURRENT_DISTS
+ARCHS="i386 amd64 powerpc"
+processDist "etch"
+
+ARCHS="i386 amd64 powerpc"
+processDist "lenny"
