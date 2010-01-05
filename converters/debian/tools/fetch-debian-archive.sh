a7c8f68e9601136e990b17208f11d60f9cb217df
Nacho Barrientos <nacho@criptonita.com>
2010-01-05 16:40:24 +0100

    Add support for download from older and current distributions

--- converters/debian/tools/fetch-debian-archive.sh
+++ converters/debian/tools/fetch-debian-archive.sh
@@ -1,24 +1,43 @@
 #!/bin/bash
 
-DISTS="slick potato woody sarge"
+ARCHIVED_DISTS="slink potato woody sarge"
+CURRENT_DISTS="etch lenny"
 ARCHS="i386"
 AREAS="main non-free contrib"
-BASE_URL="http://ftp.de.debian.org/archive/debian/dists"
 OUTPUT_DIR="dists"
 
-for dist in $DISTS; do
-	echo "Voy a descargarme todo sobre $dist"
-	for area in $AREAS; do
-	    echo "Entramos en el area $area"
-	    SOURCES_URL="$BASE_URL/$dist/$area/source/Sources.gz"
-	    echo "Descargando sources: $SOURCES_URL"
-	    mkdir -p $OUTPUT_DIR/$dist/$area/source
-	    wget $SOURCES_URL -O $OUTPUT_DIR/$dist/$area/source/Sources.gz
-	    for arch in $ARCHS; do
-	        ARCH_URL="$BASE_URL/$dist/$area/binary-$arch/Packages.gz"
-	        echo "Descargando arquitectura $ARCH_URL"
-	        mkdir -p $OUTPUT_DIR/$dist/$area/binary-$arch
-	        wget $ARCH_URL -O $OUTPUT_DIR/$dist/$area/binary-$arch/Packages.gz
-        done
-	done
-done
+function processDists {
+  for dist in $@; do
+    echo "Will download $dist (arches: $ARCHS)"
+    for area in $AREAS; do
+        echo "Area: $area"
+        mkdir -p $OUTPUT_DIR/$dist/$area/source
+        SOURCES_URL="$BASE_URL/$dist/$area/source/Sources.gz"
+        OUTPUT_PATH="$OUTPUT_DIR/$dist/$area/source/Sources.gz"
+        processFile $SOURCES_URL $OUTPUT_PATH
+        for arch in $ARCHS; do
+            mkdir -p $OUTPUT_DIR/$dist/$area/binary-$arch
+            PACKAGES_URL="$BASE_URL/$dist/$area/binary-$arch/Packages.gz"
+            OUTPUT_PATH="$OUTPUT_DIR/$dist/$area/binary-$arch/Packages.gz"
+            processFile $PACKAGES_URL $OUTPUT_PATH
+          done
+    done
+  done
+}
+
+function processFile {
+  # $1: Download URL $2: Output file
+  echo "Downloading file: $1..."
+  wget $1 -O $2 &> /dev/null
+  local OUTPUT_PATH_UNZIPPED=$(echo $2 |sed -e "s/.gz//")
+  echo "Gunzipping and recoding $2 to $OUTPUT_PATH_UNZIPPED..."
+  gzip -d $2 -c > $OUTPUT_PATH_UNZIPPED
+  iconv -f "ISO-8859-1" -t "UTF-8" $OUTPUT_PATH_UNZIPPED > $OUTPUT_PATH_UNZIPPED.u
+  mv $OUTPUT_PATH_UNZIPPED.u $OUTPUT_PATH_UNZIPPED
+}
+ 
+BASE_URL="http://ftp.de.debian.org/archive/debian/dists"
+processDists $ARCHIVED_DISTS
+
+BASE_URL="http://ftp.de.debian.org/debian/dists"
+processDists $CURRENT_DISTS
--- converters/debian/tools/process-archive.sh
+++ converters/debian/tools/process-archive.sh
@@ -3,5 +3,5 @@ OUTDIR=$2
 DISTRIBUTIONS="slink potato woody sarge etch lenny"
 for d in $DISTRIBUTIONS;
 do
-	bash process-archive.sh $DISTDIR $d $OUTDIR;
+	bash process-distribution.sh $DISTDIR $d $OUTDIR;
 done
