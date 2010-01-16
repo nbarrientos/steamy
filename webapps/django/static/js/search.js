0332fee4470823c45d0afd6cfad935ba9bda864b
Nacho Barrientos <nacho@criptonita.com>
2010-01-16 14:26:42 +0100

    Prevent searching in package descriptions if exact match is enabled

--- webapps/django/static/js/search.js
+++ webapps/django/static/js/search.js
@@ -4,6 +4,10 @@ $(function() {
 
     $('#id_exactmatch').click(function() {
         if ($('#id_exactmatch').is(':checked')) {
+            if ($('#id_searchtype_2').is(':checked')) {
+                $("#id_searchtype_2").removeAttr("checked");
+                $("#id_searchtype_0").attr("checked", "checked");
+            }
             $("#id_searchtype_2").attr("disabled", "disabled");
         } else {
             $("#id_searchtype_2").removeAttr("disabled");
