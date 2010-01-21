c2e38428b275312f3e1887ef999bffcbf9252cb6
Nacho Barrientos <nacho@criptonita.com>
2010-01-21 21:51:20 +0100

    PEP8izing method names

--- enrichers/debian/constraints.py
+++ enrichers/debian/constraints.py
@@ -27,11 +27,11 @@ class ConstraintResolver():
   def __init__(self):
     self.opts = None
 
-  def initData(self):
+  def _init_data(self):
     self.endpoint = SPARQLWrapper2(self.opts.endpoint)
     self.pool = GraphPool(self.opts.pool, self.opts.prefix, self.opts.basedir)
 
-  def parseArgs(self):
+  def _parse_args(self):
     parser = OptionParser(usage="%prog [options]", version="%prog " + VERSION)
     parser.add_option("-e", "--endpoint", dest="endpoint",\
                       metavar="URI", help="use URI as SPARQL endpoint")
@@ -59,7 +59,7 @@ class ConstraintResolver():
     elif self.opts.verbose and self.opts.quiet:
       raise Exception("Verbose (-v) and Quiet (-q) are mutually exclusive")
 
-  def configLogger(self):
+  def _config_logger(self):
     if self.opts.verbose:
       lvl = logging.DEBUG
     elif self.opts.quiet:
@@ -71,22 +71,22 @@ class ConstraintResolver():
 
   def run(self):
     try:
-      self.parseArgs()
+      self._parse_args()
     except Exception, e:
       print >> sys.stderr, str(e)
       sys.exit(2)
 
-    self.configLogger()
-    self.initData()
-    self.resolveConstraints()
+    self._config_logger()
+    self._init_data()
+    self._resolver_constraints()
 
   ##### Logic #####
 
-  def queryEndpoint(self, query):
+  def _query_endpoint(self, query):
     self.endpoint.setQuery(query)
     return self.endpoint.query()
 
-  def getCandidateLinksBindings(self):
+  def _get_candidate_links_bindings(self):
     q = """
         PREFIX deb: <http://idi.fundacionctic.org/steamy/debian.owl#>
         SELECT ?b ?c ?bvn ?cvn ?op
@@ -104,13 +104,13 @@ class ConstraintResolver():
         }
     """ % ("FROM <%s>" % self.opts.graph if self.opts.graph is not None else "")
     
-    r = self.queryEndpoint(q)
+    r = self._query_endpoint(q)
     return r["b", "c", "bvn"]
 
-  def resolveConstraints(self):
+  def _resolver_constraints(self):
     counter = 0
 
-    for b in self.getCandidateLinksBindings():
+    for b in self._get_candidate_links_bindings():
         counter = counter + 1
         binaryPackage = b["b"].value
         constraint = b["c"].value
@@ -126,19 +126,19 @@ class ConstraintResolver():
           constraintOperator = None
         
         if constraintVersion and constraintOperator:
-          cvObject = self.composeVersion(constraintVersion)
-          bvObject = self.composeVersion(binaryPackageVersion)
-          if self.isVersionedConstraintSatisfied(cvObject, bvObject, constraintOperator):
-            self.satisfyConstraint(binaryPackage, constraint)
+          cvObject = self._compose_version(constraintVersion)
+          bvObject = self._compose_version(binaryPackageVersion)
+          if self.is_versioned_constraint_satisfied(cvObject, bvObject, constraintOperator):
+            self._satisfy_constraint(binaryPackage, constraint)
         else:
-            self.satisfyConstraint(binaryPackage, constraint)
+            self._satisfy_constraint(binaryPackage, constraint)
 
     logging.info("Satisfied %s dependencies out of %s possible matches" % \
     (self.pool.count_triples(), counter))
 
     self.pool.serialize()
     
-  def composeVersion(self, versionURI):
+  def _compose_version(self, versionURI):
     q = """
         PREFIX deb: <http://idi.fundacionctic.org/steamy/debian.owl#>
         SELECT ?v
@@ -147,20 +147,20 @@ class ConstraintResolver():
           <%s> deb:fullVersion ?v .
         }
     """ % ("FROM <%s>" % self.opts.graph if self.opts.graph is not None else "", versionURI)
-    r = self.queryEndpoint(q)
+    r = self._query_endpoint(q)
 
     v = r["v"][0]["v"].value
 
     return Version(v)
 
-  def isVersionedConstraintSatisfied(self, constraintVersion, packageVersion, op):
+  def is_versioned_constraint_satisfied(self, constraintVersion, packageVersion, op):
     mappings = {'=':'==', '>>':'>', '<<':'<', '<':'<=', '>':'>='}
     if op in mappings:
       op = mappings[op]
 
     return eval("packageVersion %s constraintVersion" % op)
 
-  def satisfyConstraint(self, binaryPackage, constraint):
+  def _satisfy_constraint(self, binaryPackage, constraint):
     logging.debug("True! Adding <%s> deb:satisfies <%s>" % \
     (binaryPackage, constraint))
     self.pool.add_triple((URIRef(binaryPackage), DEB['satisfies'], URIRef(constraint)))
